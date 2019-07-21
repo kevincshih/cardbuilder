530e23056117573f51313dfefe9660d57e0b6c45
Kevin Shih <kevincshih@gmail.com>
2019-07-21 14:20:10 -0700

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+  <textarea name="txtinput">Enter text here...</textarea>
+  <button>Submit</button>
+  `
+})
+export class AppComponent {
+
+}
